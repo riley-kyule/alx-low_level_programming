e83bbd87a3163b5f8d5bacf7a4ed2b51318b98f8
riley_kyule <phillipkyule@gmail.com>
2022-07-01 02:20:19 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This prints the phrase "Programming is like bulding a Multilingual Puzzle"
+ * Return - Exits the program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a Multilingual puzzle");
+	return (0);
+}

20ec4e7a0216ca98e3550b77a9643c2d9e57e008
riley_kyule <phillipkyule@gmail.com>
2022-07-02 18:47:27 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	print f("\n");
+	return (0);
+}

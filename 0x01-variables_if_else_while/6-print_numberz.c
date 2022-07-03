c087d6d4429ecba89ec8eee0f2dee2dd2d3e6692
riley_kyule <phillipkyule@gmail.com>
2022-07-02 18:52:40 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/** 
+ * main - Entry point
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

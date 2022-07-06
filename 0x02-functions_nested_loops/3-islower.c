dd43f6b8e6bf92f40be6792c2ed9abe4c76630a8
riley_kyule <phillipkyule@gmail.com>
2022-07-05 18:49:13 -0700

    4. isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase characters
- * c: Argument of function
+ * @c: Argument of function
  * Return: 0 || 1 for success
  */
 int _islower(int c)
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabet characters
+ * @c: int used for argument function
+ * Return: 0 || 1
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

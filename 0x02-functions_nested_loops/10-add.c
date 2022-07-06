3dadd5188286bf09226bb230ed93a5bf140a6944
riley_kyule <phillipkyule@gmail.com>
2022-07-05 19:41:14 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: First argument function
+ * @b: second arguemnet function
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int _abs(int c);
 int print_last_digit(int x);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 
 #endif

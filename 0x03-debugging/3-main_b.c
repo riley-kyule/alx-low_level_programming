d2ab641a0ca3d487ee507db7fda88c7f38db6fc2
riley_kyule <phillipkyule@gmail.com>
2022-07-07 19:59:33 -0700

    3. Leap year

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+#include "main.h"
+
+/**
+ * main - Prints remaing dates in the year
+ * Return: 0
+ */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month = 2;
+	day = 29;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}

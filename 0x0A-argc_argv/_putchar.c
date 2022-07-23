be6a693ade209cf66bd8bdda5fa1a073bd663087
riley_kyule <phillipkyule@gmail.com>
2022-07-22 17:01:37 -0700

    Putchar

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to be printed
+ * Return: 1 on Success
+ * -1 on error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

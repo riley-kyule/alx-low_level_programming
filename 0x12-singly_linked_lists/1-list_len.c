63dbf47e1915ba4cf6253ff580a0d693e98b486f
riley_kyule <phillipkyule@gmail.com>
2022-08-11 12:45:56 -0700

    Removed excess declaration

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,22 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * list_len - Finds the number of elements in
  *            a linked list_t list.
  * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
  *
  * Return: The number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	size_t elements = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		elements++;
+		n++;
 		h = h->next;
 	}
 
-	return (elements);
+	return (n);
 }
